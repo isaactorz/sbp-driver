692dda82a1342e615d88c3b237e5d8c610b2470b
Isaac Torres <isaac.torres@swift-nav.com>
2021-10-08 12:02:35 -0600

    Second sbp doc example

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,21 @@
-use std::convert::TryFrom;
 use std::error::Error;
 use std::io;
 use std::process;
 
-use sbp::messages::logging::MsgLog;
+use sbp::{SbpEncoder, SbpMessage};
 
-fn example() -> Result<(), Box<dyn Error>> {
+fn example(sender_id: u16) -> Result<(), Box<dyn Error>> {
     let messages = sbp::iter_messages(io::stdin());
-    for msg in messages {
-        let msg = msg?; // ?
-        match MsgLog::try_from(msg) {
-            Ok(msg) => eprintln!("{}", msg.text),
-            _ => {}
-        }
-    }
+    let messages = messages.filter_map(|msg| match msg {
+        Ok(msg) if msg.sender_id() == Some(sender_id) => Some(msg),
+        _ => None,
+    });
+    SbpEncoder::new(io::stdout()).send_all(messages)?;
     Ok(())
 }
 
 fn main() {
-    if let Err(err) = example() {
+    if let Err(err) = example(31183) {
         eprintln!("error: {}", err);
         process::exit(1);
     }
