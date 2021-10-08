e99e8bcf7bf33d48f75bbbd83613e61342b349ee
Isaac Torres <isaac.torres@swift-nav.com>
2021-10-08 14:29:53 -0600

    Calculate Average cn0 for Satellite

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,30 @@ use std::process;
 
 use sbp::Sbp;
 
-fn example() -> Result<(), Box<dyn Error>> {
+fn average_cn0(sat: u8) -> Result<(), Box<dyn Error>> {
     let messages = sbp::iter_messages(io::stdin());
+    let mut acc: u64 = 0;
+    let mut count: u64 = 0;
     for msg in messages {
         let msg = msg?;
         if let Sbp::MsgObs(msg_obs) = msg {
             for ob in msg_obs.obs {
-                let cn0 = ob.cn0;
                 let sid = &ob.sid;
-                let sat = sid.sat;
-                eprintln!("SatID: {}, cn0: {}", sat, cn0);
+                if sat == sid.sat {
+                    acc += ob.cn0 as u64;
+                    count += 1;
+                }
             }
         }
     }
+
+    let average = acc as f64 / count as f64;
+    println!("Average cn0 of Sat {}: {}", sat, average);
     Ok(())
 }
 
 fn main() {
-    if let Err(err) = example() {
+    if let Err(err) = average_cn0(8) {
         eprintln!("error: {}", err);
         process::exit(1);
     }
